946b76c51da80f305331c089209e787612e414ab
Johnathan <johnathanmiddleton1@gmail.com>
2022-05-31 16:21:06 -0700

    moved location up

--- src/App.js
+++ src/App.js
@@ -47,7 +47,10 @@ function App() {
                     setLng={setLng}
                     setFormattedLoc={setFormattedLoc}
                 />
-                <h3>Help</h3>
+                {formattedLoc !== "" && (
+                    <h4>Currently Viewing: {formattedLoc}</h4>
+                )}
+                <h4>Help</h4>
                 <p>AQI is air quality index, 1-5 - 1 is best.<br />
                 Clouds is a percentage, 30%-70% is best.<br />
                 Humidity is a percentage, lower percentages are best.<br />
@@ -55,9 +58,6 @@ function App() {
                 </p>
 
                 <Weather lat={lat} lng={lng} />
-                {formattedLoc !== "" && (
-                    <h3>Currently Viewing: {formattedLoc}</h3>
-                )}
             </header>
         </div>
     );
